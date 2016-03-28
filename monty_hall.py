a63a3894b0f66c786a4f01faa3b931020c793481
Kamal Wood <kamalwood@hotmail.com>
2016-03-28 22:47:14 +0100

    Now correctly initialises game space at start of each run.

--- monty_hall.py
+++ monty_hall.py
@@ -11,30 +11,27 @@ class Result:
         self.random = random
 
 class Game:
-    def __init__(self):
-        """Set up the game. A 0 is a goat, a 1 is the car."""
-        self.choices = [0, 0, 1]
-        rand.shuffle(self.choices)
-
     def make_first_choice(self):
-        """Choose an index denoting the first choice in the choices."""
+        """Randomly choose an index to make the initial choice."""
         return rand.choice([0, 1, 2])
 
     def run(self, i):
         """Run the game:
 
-        0. The choices have already been randomly initialised.
-        1. Make the first choice and save its result.
-        2. Make the switch choice and save its result.
-        3. Make the random switch choice value, and save its result.
+        1. Randomly initialise the choices.
+        2. Randomly make the first choice and save its result.
+        3. Make the switch choice and save its result.
+        4. Make the random switch choice, and save its result.
 
         The parameter i is required by pool.map to run multiple instances, but
         isn't required in each individual game run."""
+        choices = [0, 0, 1]
+        rand.shuffle(choices)
 
         first_choice_index = self.make_first_choice()
-        first_choice_value = self.choices[first_choice_index]
+        first_choice_value = choices[first_choice_index]
 
-        switch_value = 0 if first_choice_value else 1
+        switch_value = 0 if first_choice_value == 1 else 1
         random_value = rand.choice([first_choice_value, switch_value])
 
         return Result(first_choice_value, switch_value, random_value)
@@ -42,7 +39,7 @@ class Game:
 
 def get_num_runs_chunk(num_tries, chunk_size):
     """A generator to help split up the number of tries into manageable chunks
-    so that the lsit of results doesn't get too big."""
+    so that the list of results doesn't get too big."""
     if chunk_size >= num_tries:
         yield num_tries
     else:
@@ -81,7 +78,7 @@ def main():
             final_result.switch += result.switch
             final_result.random += result.random
 
-    print(locale.format_string("\nAlways keeping first resulted in %d cars, or cars %.2f%% of the time.", (final_result.first, final_result.first/num_tries * 100), grouping=True))
+    print(locale.format_string("\nAlways keeping first choice resulted in %d cars, or cars %.2f%% of the time.", (final_result.first, final_result.first/num_tries * 100), grouping=True))
     print(locale.format_string("Always switching resulted in %d cars, or cars %.2f%% of the time.", (final_result.switch, final_result.switch/num_tries * 100), grouping=True))
     print(locale.format_string("Randomly switching resulted in %d cars, or cars %.2f%%  of the time.\n", (final_result.random, final_result.random/num_tries * 100), grouping=True))
 
