ea6cbadff0606a67c7881a74ec4788898bc6afd5
Kamal Wood <kamalwood@hotmail.com>
2016-03-28 22:25:08 +0100

    Comments.

--- monty_hall.py
+++ monty_hall.py
@@ -4,14 +4,14 @@ from multiprocessing import Pool
 rand = random.SystemRandom()
 
 class Result:
-    def __init__(self, first=0.0, switch=0.0, r=0.0):
+    def __init__(self, first=0, switch=0, random=0):
         self.first = first
         self.switch = switch
-        self.random = r
+        self.random = random
 
 class Game:
     def __init__(self):
-        """Sets up the game. A 0 in the choice is a goat, a 1 is the car."""
+        """Set up the game. A 0 is a goat, a 1 is the car."""
         self.choices = [0, 0, 1]
         rand.shuffle(self.choices)
 
@@ -20,21 +20,54 @@ class Game:
         return rand.choice([0, 1, 2])
 
     def run(self, i):
-        first_choice_index = self.make_first_choice()
+        """Run the game:
 
+        0. The choices have already been randomly initialised.
+        1. Make the first choice and save its result.
+        2. Make the switch choice and save its result.
+        3. Make the random switch choice value, and save its result.
+
+        The parameter i is required by pool.map to run multiple instances, but
+        isn't required in each individual game run."""
+
+        first_choice_index = self.make_first_choice()
         first_choice_value = self.choices[first_choice_index]
+
         switch_value = 0 if first_choice_value else 1
-        if rand.choice([True, False]):
-            random_value = switch_value
-        else:
-            random_value = first_choice_value
+        random_value = rand.choice([first_choice_value, switch_value])
 
         return Result(first_choice_value, switch_value, random_value)
 
 
+def get_num_runs_chunk(num_tries, chunk_size):
+    """A generator to help split up the number of tries into manageable chunks
+    so that the lsit of results doesn't get too big."""
+    if chunk_size >= num_tries:
+        yield num_tries
+    else:
+        num_chunks = math.floor(num_tries / chunk_size)
+        remainder = num_tries % chunk_size
+
+        for i in range(num_chunks):
+            yield chunk_size
+
+        if remainder != 0:
+            yield remainder
+
+
+def get_args():
+    """Get the one optional command-line arg."""
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--num_tries", help="Number of tries.", default="10**6")
+    args = parser.parse_args()
+
+    return eval(args.num_tries)
+
+
 def main():
     num_tries = get_args()
-    chunk_size = 10**6
+    print("\nSimulating game {0:d} times.".format(num_tries))
+    chunk_size = 10**4
 
     game = Game()
     pool = Pool()
@@ -47,35 +80,10 @@ def main():
             final_result.switch += result.switch
             final_result.random += result.random
 
-    print("Always keeping first resulted in {} cars, or cars {0:.2f}%% of the time.".format(final_result.first, final_result.first/num_tries * 100))
-    print("Always switching resulted in {} cars, or cars {0:.2f}%% of the time.".format(final_result.switch, final_result.switch/num_tries * 100))
-    print("Randomly switching resulted in {} cars, or cars {0:.2f}%% of the time.".format(final_result.random, final_result.random/num_tries * 100))
+    print("\nAlways keeping first resulted in {0:d} cars, or cars {1:.2f}% of the time.".format(final_result.first, final_result.first/num_tries * 100))
+    print("Always switching resulted in {0:d} cars, or cars {1:.2f}% of the time.".format(final_result.switch, final_result.switch/num_tries * 100))
+    print("Randomly switching resulted in {0:d} cars, or cars {1:.2f}% of the time.\n".format(final_result.random, final_result.random/num_tries * 100))
 
-
-def get_args():
-    """Get the one positional command-line arg."""
-    parser = argparse.ArgumentParser()
-    parser.add_argument("num_tries", help="Number of tries.")
-    args = parser.parse_args()
-
-    return eval(args.num_tries)
-
-
-def get_num_runs_chunk(num_tries, chunk_size):
-    """A generator for yielding the size of each number of parallel runs.
-    This is done to prevent the memory footprint from the number of results
-    getting too big."""
-    if chunk_size >= num_tries:
-        yield num_tries
-    else:
-        num_chunks = math.floor(num_tries / chunk_size)
-        remainder = num_tries % chunk_size
-
-        for i in range(num_chunks):
-            yield chunk_size
-
-        if remainder != 0:
-            yield remainder
 
 if __name__ == '__main__':
     import time
